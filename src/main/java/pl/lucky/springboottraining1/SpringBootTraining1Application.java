a34b67dbf6556ed3cda16712e475a40675c009dc
lucki90 <tomlucki@wp.pl>
2019-06-13 18:49:18 +0200

    first commit

---
+++ src/main/java/pl/lucky/springboottraining1/SpringBootTraining1Application.java
@@ -0,0 +1,13 @@
+package pl.lucky.springboottraining1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTraining1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTraining1Application.class, args);
+    }
+
+}
